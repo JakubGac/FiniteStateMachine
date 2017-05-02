d8e11914519c787a87a94ce60652f74a7e523588
jakubgac <gacus65@gmail.com>
2017-05-02 13:57:40 +0200

    Initial Commit

---
+++ Automat/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Automat
+//
+//  Created by Jakub Gac on 02.05.2017.
+//  Copyright © 2017 Jakub Gac. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
